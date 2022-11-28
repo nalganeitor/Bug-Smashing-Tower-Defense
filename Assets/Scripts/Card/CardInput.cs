d46afe71a5565a0f88b4ae421b29ed935e6912ac
Marco199609 <marco19-96@hotmail.com>
2022-11-27 22:01:09 -0300

    Card Input

--- Assets/Scripts/Card/CardInput.cs
+++ Assets/Scripts/Card/CardInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler
+public class CardInput : MonoBehaviour, IPointerDownHandler, IDragHandler, IBeginDragHandler, IEndDragHandler, IDropHandler
 {
     private RectTransform rectTransform;
 
