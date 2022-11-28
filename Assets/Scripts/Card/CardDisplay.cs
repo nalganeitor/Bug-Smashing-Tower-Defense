ec722729c249ea230c3ba804e633ef2bc4eac428
Marco199609 <marco19-96@hotmail.com>
2022-11-28 06:16:10 -0300

    Refactoring card logic

--- Assets/Scripts/Card/CardDisplay.cs
+++ Assets/Scripts/Card/CardDisplay.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 public class CardDisplay : MonoBehaviour
 {
     public CardData card;
-    public Image artwork;
+    public Sprite artwork;
     public Text cardName;
     public Text powerText;
     public Text costText;
     // Start is called before the first frame update
     void Start()
     {
-        artwork.sprite = card.artwork;
+        artwork = card.artwork;
         cardName.text = card.name;
         powerText.text = card.power.ToString();
         costText.text = card.cost.ToString();
