0e9036f70366b2814d924919b0dc6d4e161dd8ff
Marco199609 <marco19-96@hotmail.com>
2022-11-27 21:04:59 -0300

    Made card stats

--- Assets/Assets/Cards/CardDisplay.cs
+++ Assets/Assets/Cards/CardDisplay.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
     public CardData card;
+    public Image artwork;
+    public Text cardName;
+    public Text powerText;
+    public Text costText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        artwork.sprite = card.artwork;
+        cardName.text = card.name;
+        powerText.text = card.power.ToString();
+        costText.text = card.cost.ToString();
     }
 }
