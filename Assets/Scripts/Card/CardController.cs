2c12aafb4b5a749c3a5e2383d449cd0a330c751f
Marco199609 <marco19-96@hotmail.com>
2022-11-30 02:03:30 -0300

    All 4 cards working with basic interaction

--- Assets/Scripts/Card/CardController.cs
+++ Assets/Scripts/Card/CardController.cs
@@ -15,7 +15,7 @@ public class CardController : MonoBehaviour
 
     void Update()
     {
-        DragAndDropCard();
+        MouseCardControl();
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -33,29 +33,35 @@ public class CardController : MonoBehaviour
     }
 
 
-    void DragAndDropCard()
+    void MouseCardControl()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+        Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
 
-            if (targetObject == GetComponent<Collider2D>())
+        if (targetObject == GetComponent<Collider2D>())
+        {
+            if (Input.GetMouseButtonDown(0))
             {
                 selectedObject = targetObject.transform.gameObject;
                 offset = selectedObject.transform.position - mousePosition;
             }
+            else
+                 gameObject.transform.localScale = new Vector2(1.2f, 1.2f);
         }
         else
+            gameObject.transform.localScale = new Vector2(1f, 1f);
+
+        if (!Input.GetMouseButtonDown(0))
         {
-            print("wow");
             transform.position = Vector2.Lerp(transform.position, cardOrigin.position, Time.deltaTime * backToOriginSpeed);
         }
+
         if (selectedObject)
         {
             selectedObject.transform.position = mousePosition + offset;
         }
+
         if (Input.GetMouseButtonUp(0) && selectedObject)
         {
            selectedObject = null;
